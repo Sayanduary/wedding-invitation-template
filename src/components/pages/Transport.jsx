15a1028f7fa9709814841ad32d0fa716c64d1cea
Sayan Duary <sayanduary@gmail.com>
2026-04-10 21:01:06 +0530

    refactor: Add justify-center class to Transport component and remove transport RSVP note

--- src/components/pages/Transport.jsx
+++ src/components/pages/Transport.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Transport() {
   return (
-    <div className="min-h-screen h-auto flex flex-col gap-4 sm:gap-5 items-center bg-[#faf8f5] px-6 py-10 sm:p-20 text-center">
+    <div className="min-h-screen h-auto flex flex-col gap-4 sm:gap-5 items-center bg-[#faf8f5] px-6 py-10 sm:p-20 text-center justify-center">
       <p className="text-[#5c2018] text-sm">HOW TO REACH US</p>
       <h1 className="text-3xl sm:text-4xl text-[#5c2018] text-spacing-wide">
         Transport
@@ -15,9 +15,6 @@ function Transport() {
       <p className="text-[#5c2018] mt-8 sm:mt-10 ">BUS DEPARTURE</p>
       <p className="text-[#5c2018] text-sm sm:text-base">
         10:00 AM - Piazza della Repubblica, Florence
-      </p>
-      <p className="text-[#5c2018] mt-8 sm:mt-10 font-['Playfair_Display'] italic text-2xl sm:text-3xl max-w-3xl">
-        Please indicate in your RSVP if you need transport
       </p>
     </div>
   );
